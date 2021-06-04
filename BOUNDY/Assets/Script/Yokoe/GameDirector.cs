b0c4ca53da40f2845651e34993ec5e4477b65a33
ShunyaYokoe <nkcgta.shunya.yokoe@gmail.com>
2021-06-04 11:24:24 +0900

    Prefab作成

--- BOUNDY/Assets/Script/Yokoe/GameDirector.cs
+++ BOUNDY/Assets/Script/Yokoe/GameDirector.cs
@@ -12,7 +12,35 @@ public class GameDirector : MonoBehaviour
     /// <summary>
     /// ばねの数
     /// </summary>
-    public static int Spring_ct = 0;
+    public static int Spring_ct { get; set; } = 0;
+
+    /// <summary>
+    /// 生成した回数(合計)
+    /// </summary>
+    int spring_createnum = 0;
+
+    /// <summary>
+    /// 次に生成するばねの番号
+    /// </summary>
+    int spring_nextnum = 0;
+
+    bool spring_key;
+
+    /// <summary>
+    /// ばねがおける最大数
+    /// </summary>
+    const int Max_spring = 1;
+
+    /// <summary>
+    /// 現在のばねの数
+    /// </summary>
+    GameObject[] Springs =new GameObject[Max_spring];
+
+    /// <summary>
+    /// 生成したばねの番号
+    /// </summary>
+    int[] Create_num = new int[Max_spring];
+
 
     // Start is called before the first frame update
     void Start()
@@ -25,20 +53,20 @@ public class GameDirector : MonoBehaviour
     {
         if(Input.GetMouseButton(0))
         {
-            //長押ししている間ジャンプ力を上げる
-            SpringCon.jumpforce.y += 0.1f;
+            //  長押ししている間ジャンプ力を上げる
+            PlayerCon.jumpforce.y += 0.1f;
             Time.timeScale = 0.3f;
         }
 
         if(Input.GetMouseButtonDown(0))
         {
-            //マウスの座標取得
+            //  マウスの座標取得
             Vector3 mouse_pos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Mathf.Abs(Camera.main.transform.position.z));
-            //ワールド座標に変換
+            //  ワールド座標に変換
             Vector3 world_pos = Camera.main.ScreenToWorldPoint(mouse_pos);
 
             
-            //ばねが１つ以上置かれているときばねを消す
+            //  ばねが１つ以上置かれているときばねを消す
             if (Spring_ct != 0)
             {
                 Spring_ct = 0;
@@ -46,8 +74,15 @@ public class GameDirector : MonoBehaviour
             }
 
             Spring_ct += 1;
-            //マウスの座標にオブジェクト生成
-            Instantiate(Spring, world_pos, Quaternion.identity);
+            //  マウスの座標にオブジェクト生成
+            Springs[spring_nextnum] = Instantiate(Spring, world_pos, Quaternion.identity);
+            if(spring_createnum<Max_spring)
+            {
+                spring_nextnum++;
+
+            }
+
+
         }
 
         if (Input.GetMouseButtonUp(0))
--- BOUNDY/Assets/Script/Yokoe/SpringCon.cs
+++ BOUNDY/Assets/Script/Yokoe/SpringCon.cs
@@ -5,40 +5,207 @@ using UnityEngine;
 public class SpringCon : MonoBehaviour
 {
     /// <summary>
-    /// ジャンプ力
+    /// ばねのプレハブ
     /// </summary>
-    public static Vector2 jumpforce = new Vector2(0.0f,5.0f);
+    [SerializeField] GameObject Spring = null;
 
     /// <summary>
-    /// プレイヤーに触れた回数
+    /// 生成できるばねの最大数
     /// </summary>
-    int pl_touchct = 0;
+    const int Max_springs = 3;
 
-    private void OnCollisionEnter2D(Collision2D col)
+    /// <summary>
+    /// 今までに生成した数（合計）
+    /// </summary>
+    int create_spring_count = 0;
+
+    /// <summary>
+    /// 現在生成されているばねの合計数
+    /// </summary>
+    int now_create_num = 0;
+
+   /// <summary>
+   /// 1つ前に生成したばねの配列番号
+   /// </summary>
+    int past_spring_num = 0;
+
+    /// <summary>
+    /// 1つ前に生成したばねのSpriteRenderer
+    /// </summary>
+    SpriteRenderer past_spring_sprr = null;
+
+    /// <summary>
+    /// 次にばねを生成するときの配列番号
+    /// </summary>
+    int next_spring_num = 0;
+
+    /// <summary>
+    /// 生成したばね
+    /// </summary>
+    GameObject[] Springs = new GameObject[Max_springs];
+
+    /// <summary>
+    /// 生成したばねの履歴
+    /// </summary>
+    int[] history_spring =new int[Max_springs];
+
+    /// <summary>
+    /// 生成したばねの強さ
+    /// </summary>
+    float[] springs_value = new float[Max_springs];
+
+    /// <summary>
+    /// プレイヤーが使っているばねの番号(使っていない場合：-1)
+    /// </summary>
+    private int use_spring_num = -1;
+
+    /// <summary>
+    /// 指定されたばねの強さを取得
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public float GetSpringpower(int n)
     {
-        //当たったオブジェクトのタグがプレイヤーの場合
-        if(col.gameObject.CompareTag("Player"))
+        use_spring_num = n;
+        return 1 / 0.3f * springpower;
+    }
+
+     /// <summary>
+     /// 履歴を指定された箇所から左詰めにする
+     /// </summary>
+     /// <param name="n"></param>
+    void LeftHistory(int n)
+    {
+        for (int i = n; i < Max_springs - 1; ++i) 
         {
-            col.gameObject.GetComponent<Rigidbody2D>().AddForce(jumpforce, ForceMode2D.Impulse);
-            jumpforce.y = 5.0f;
-            pl_touchct += 1;
+            history_spring[i] = history_spring[i + 1];
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// 履歴の中の指定された箇所を削除
+    /// </summary>
+    /// <param name="n"></param>
+    public void DeleteHistory(int n)
     {
-        
+        int breakpoint = 0;
+        for(int i=0;i<Max_springs;++i)
+        {
+            if(history_spring[i]==n)
+            {
+                breakpoint = i;
+                break;
+            }
+        }
+        LeftHistory(breakpoint);
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// ばねを生成する
+    /// </summary>
+    void Create_Spring()
     {
-        if(pl_touchct==1)
+       //最大まで生成されている場合1番古いものを消す
+       if(now_create_num>=Max_springs)
         {
-            Destroy(gameObject);
-            pl_touchct = 0;
-            GameDirector.Spring_ct = 0;
+            if(use_spring_num!=-1&&history_spring[0]==use_spring_num)
+            {
+                Destroy(Springs[history_spring[1]].gameObject);
+                next_spring_num = history_spring[1];
+                LeftHistory(history_spring[2]);
+            }
+            else
+            {
+                Destroy(Springs[history_spring[0]].gameObject);
+            }
+            --now_create_num;
+        }
+
+        //ばねの生成
+        Springs[next_spring_num] = Instantiate(Spring);
+        Springs[next_spring_num].name = next_spring_num.ToString();
+        past_spring_sprr = Springs[next_spring_num].GetComponent<SpriteRenderer>();
+        past_spring_num = next_spring_num;
+
+        //生成したばねの位置変更
+        Vector3 touchpos = Camera.main.ScreenToWorldPoint(Input.mousePosition - Camera.main.transform.position);
+        Springs[past_spring_num].transform.position = touchpos;
+
+        //生成した回数を加算
+        create_spring_count++;
+        now_create_num++;
+
+        //履歴を更新
+        //履歴をもとに次生成する際の配列の番号を決定
+        if(create_spring_count<=Max_springs)
+        {
+            history_spring[next_spring_num] = next_spring_num;
+            next_spring_num++;
+            if(next_spring_num==Max_springs)
+            {
+                next_spring_num = 0;
+            }
+        }
+        else
+        {
+            LeftHistory(0);
+            history_spring[Max_springs - 1] = next_spring_num;
+            next_spring_num = history_spring[0];
+        }
+
+        Debug.Log(history_spring[0] + "_" + history_spring[1] + "_" + history_spring[2]);
+    }
+
+    public void DeleteSpring(int n)
+    {
+        //対象を削除
+        Destroy(Springs[n].gameObject);
+
+        //生成されている回数減算
+        now_create_num--;
+
+        //履歴から対象の番号を削除
+        DeleteHistory(n);
+
+        //次に生成する際の配列の番号を更新
+        next_spring_num = n;
+
+        //ばねを使用していない状態
+        use_spring_num = -1;
+    }
+
+    void Awake()
+    {
+        //マルチタッチ無効化
+        Input.multiTouchEnabled = false;
+    }
+
+    /// <summary>
+    /// ばねの強さ
+    /// </summary>
+    float springpower = 0;
+
+    // Update is called once per frame
+    private void Update()
+    {
+      if(Input.GetMouseButtonDown(0))
+        {
+            Create_Spring();
+            Time.timeScale = 0.3f;
+        }
+      if(Input.GetMouseButton(0))
+        {
+            springpower = Mathf.Clamp(springpower += Time.deltaTime, 0, 0.9f);
+            springs_value[past_spring_num] = springpower;
+            //強さによる色変え
+            float col = 1 / 0.3f * springpower;
+            past_spring_sprr.color = new Color(1, 1 - col, 1 - col, 1);
+        }
+      if(Input.GetMouseButtonUp(0))
+        {
+            Time.timeScale = 1;
+            springs_value[past_spring_num] = springpower;
+            springpower = 0;
         }
     }
 }

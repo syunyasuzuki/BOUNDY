647e6925f80bdeb4e07611c27b461353b924df2a
ShunyaYokoe <nkcgta.shunya.yokoe@gmail.com>
2021-06-07 10:00:51 +0900

    フリック作成

--- BOUNDY/Assets/Script/Yokoe/Sample_Y.cs
+++ BOUNDY/Assets/Script/Yokoe/Sample_Y.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Sample_Y : MonoBehaviour
 {
+    Vector3 startmouse_pos = new Vector3(0f, 0f, 0f);
+    Vector3 endmouse_pos = new Vector3(0f, 0f, 0f);
+    float flick_ct = 0;
+    float flick_start_ct = 0;
+    float flick_end_ct = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,22 @@ public class Sample_Y : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //フリック処理
+        if(Input.GetMouseButton(0))
+        {
+            startmouse_pos = new Vector3(Input.mousePosition.x,Input.mousePosition.y, Mathf.Abs(Camera.main.transform.position.z));
+            flick_ct += Time.deltaTime;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            endmouse_pos= new Vector3(Input.mousePosition.x, Input.mousePosition.y, Mathf.Abs(Camera.main.transform.position.z));
+            flick_end_ct = flick_ct;
+
+            if (flick_end_ct - flick_start_ct < 1.0f)
+            {
+                Debug.Log("フリック");
+            }
+        }
     }
 }
